3e1fe7ec9aed7eabe94e54b2a72cc592635f3746
mesocrisetus <alexander.bolivardiaz@gmail.com>
2026-04-17 18:37:57 +0200

    Ejercicio 01 agregado

--- Basicos/0_holaMundo.cpp
+++ Basicos/0_holaMundo.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+
+// Le indicamos al compiladro que use todos los identificadores que estan dentro del espacio SDT
+using namespace std;
 int main()
 {
-    std::cout<<"Hola mundo!"<<std::endl;
+
+    cout<<"Hola mundo!"<<endl;
     
 
     return 0;
---
+++ Basicos/1_entradaDeProcesoYSalida.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Declaracion de identificadores
+    int num;
+    int res; 
+
+    //Inicializacion
+    num = 0;
+    res = 0;
+
+    //Algoritmo
+    cout<<"ingrese un numero"<<endl;
+    cin>>num;
+    res = num * 2;
+    cout<<"El doble del numero es: "<<res<<endl;
+
+    return 0;
+}
